8411215000621f91d1f48d4a22354cb44ca399d9
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-03-02 12:40:37 -0500

    adding array into set

--- src/main.js
+++ src/main.js
@@ -19,9 +19,9 @@ const womenButton = document.getElementById("womenButton");
 const repeatedTeams = mapByKey(athletesData, "team");
 const repeatedSports = mapByKey(athletesData, "sport");
 const repeatedEvents = mapByKey(athletesData, "event");
-let teams = new Set(repeatedTeams);
-let sports = new Set(repeatedSports);
-let events = new Set(repeatedEvents);
+let teams = [...new Set(repeatedTeams)];
+let sports = [...new Set(repeatedSports)];
+let events = [...new Set(repeatedEvents)];
 
 const selectTeam = document.getElementById("select-team");
 const selectSport = document.getElementById("select-sport");
@@ -123,9 +123,7 @@ selectOrder.addEventListener("change", () => {
 
 
 // Crear listas de opciones (teams y sports)
-teams = [...teams];
-sports = [...sports];
-events = [...events];
+
 
 function listOfOptions(selectCategory, list) {
     for (let i = 0; i < list.length; i++) {
